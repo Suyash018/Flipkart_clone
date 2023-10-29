c44243fb19fd507d3dbfe14d0c91feafba1ebecf
Suyash018 <picturesbysuyash@gmail.com>
2023-10-29 15:46:56 +0530

    completed the home screen ui and added products through mongodb

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -2,6 +2,14 @@
 import { Box, styled } from "@mui/material";
 import Banner from "./Banner";
 import Navbar from "./Navabar";
+import { useEffect } from "react";
+import { getProducts } from "../../redux/actions/productActions";
+
+import {useDispatch, useSelector} from 'react-redux';
+import Slide from "./Slide";
+import MidSlide from "./Midslide";
+import MidSection from "./MidSection";
+
 
 const Component = styled(Box)`
     padding :10px;
@@ -10,12 +18,47 @@ const Component = styled(Box)`
 
 const Home = () => {
 
+    const {products } = useSelector(state=> state.getProducts);
+
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(getProducts());
+    },[dispatch]);
+
     return (
 
         <>
             <Navbar />
             <Component>
                 <Banner />
+                
+                <MidSlide products={products} />
+                <MidSection />
+                <Slide
+                    data={products} 
+                    title='Discounts for You'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Suggested Items'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Top Selection'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Recommended Items'
+                    timer={false} 
+                    multi={true} 
+                />
             </Component>
 
         </>
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {  userSignUp,userLogIn } from "../controller/user-controller.js";
+import { getProducts } from "../controller/product-controller.js";
 
 
 
@@ -8,5 +9,7 @@ const router = express.Router();
 
 router.post('/signup', userSignUp);
 router.post('/login', userLogIn);
+
+router.get('/products',getProducts);
 
 export default router;
